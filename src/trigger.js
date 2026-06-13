4f0b4f576b12bd8c664020cf05661906ddce39b7
salsabilaapriani596-afk <salsabilaapriani596@gmail.com>
2026-06-13 14:27:30 +0700

    Create trigger.js
    initial trigger function

---
+++ src/trigger.js
@@ -0,0 +1,31 @@
+function beforeUpload(file){
+    console.log("Before upload:", file.name);
+}
+
+function afterUpload(file){
+    console.log("After upload:", file.name);
+}
+
+function previewFile(input){
+
+    let file = input.files[0];
+
+    if(!file) return;
+
+    beforeUpload(file);
+
+    let ext = file.name.split('.').pop().toLowerCase();
+
+    if(!["jpg","jpeg","png","pdf"].includes(ext)){
+        console.log("Format tidak valid");
+        return;
+    }
+
+    afterUpload(file);
+
+    console.log("File siap upload");
+}
+
+function deleteFile(name){
+    console.log("Delete file:", name);
+}
